84ad0ca928a94b802f2b91c1bb6645c280f58de4
conzty01 <conzty01@luther.edu>
2017-07-08 15:24:12 -0500

    Added additional pages

--- webServer.py
+++ webServer.py
@@ -7,5 +7,13 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def index():
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
