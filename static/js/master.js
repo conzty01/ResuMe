a97f87b577473f3cffbbbca64bfa50cb41eaecc9
conzty01 <conzty01@luther.edu>
2017-07-21 08:41:41 -0500

    Updated look and behavior of footer

--- static/js/master.js
+++ static/js/master.js
@@ -1,5 +1,5 @@
 // Footer form textarea adjust
 function adjust_textarea(h) {
     h.style.height = "20px";
-    h.style.height = (h.scrollHeight)+"px";
+    h.style.height = (h.scrollHeight + 5)+"px";
 }
